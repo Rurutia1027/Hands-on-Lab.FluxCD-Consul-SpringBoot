2603dc4c393657bfacd13431f9589f1b03327e1c
Rurutia1027 <rurutia1027@gmail.com>
2025-09-22 12:17:43 +0800

    update configs for spring app sync config from consul correctly

--- spring-boot-app/src/main/java/com/aston/handson/springapp/SpringBootConsulConfigApplication.java
+++ spring-boot-app/src/main/java/com/aston/handson/springapp/SpringBootConsulConfigApplication.java
@@ -1,6 +1,11 @@
 package com.aston.handson.springapp;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootConsulConfigApplication {
